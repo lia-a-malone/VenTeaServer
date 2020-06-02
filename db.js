3fa7d3660c995141f8fc9277e0201a2f602b57bb
Lia Malone <lia.a.malone@gmail.com>
2020-06-01 20:34:46 -0400

    2ndcommit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_url, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     // host: 'localhost',
     dialect: 'postgres'
 }) //creating new sequelize Object
